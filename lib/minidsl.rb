08f79d0feee2f5b3f653a2f345d707cbf3bf0a2f
Artoria <Artoria>
2016-08-24 12:42:45 +0800

    init

--- lib/minidsl.rb
+++ lib/minidsl.rb
@@ -1,5 +1,24 @@
 require "minidsl/version"
 
 module Minidsl
-  # Your code goes here...
+  def liftA(met)
+    recv, real_method = case met
+                  when Symbol then [self, method(met)]
+                  when Method then [met.receiver, met]
+                  else [nil, met]
+                  end
+    if recv
+      lambda{|*a| 
+        args = a.map{|x| from(x)} # a <- , b <- ...
+        from recv # recv <- 
+        ret real_method.call(*args)
+      }  
+    else
+      lambda{|*a| 
+        args = a.map{|x| from(x)}
+        ret real_method.call(*args)
+      }
+    end
+  end
 end
+
